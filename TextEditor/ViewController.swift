1df995acd1a7505ad816b9a27453c77ef7dbb375
azamat <azamat.kushmanov@gmail.com>
2020-03-04 23:10:54 +0600

    UI setup

--- TextEditor/ViewController.swift
+++ TextEditor/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addToolbar()
+        textView.becomeFirstResponder()
     }
 
-
+    private func addToolbar(){
+        let toolbar = UIToolbar(frame:CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        toolbar.barStyle = .default
+        toolbar.items = [
+            UIBarButtonItem(image: #imageLiteral(resourceName: "ic_indent"), style: .plain, target: self, action: #selector(tabPressed)),
+            UIBarButtonItem(image: #imageLiteral(resourceName: "ic_list"), style: .plain, target: self, action: #selector(listPressed)),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneWithNumberPad))]
+        toolbar.sizeToFit()
+        textView.inputAccessoryView = toolbar
+    }
+    
+    @objc private func tabPressed() {
+        print("tab pressed")
+    }
+    
+    @objc private func listPressed() {
+        print("list pressed")
+    }
+    
+    @objc private func doneWithNumberPad() {
+        view.endEditing(true)
+    }
 }
 
