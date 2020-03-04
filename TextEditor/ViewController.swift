a9051d7538cdb03ce50abf4e8bcd81e8ce1013b4
azamat <azamat.kushmanov@gmail.com>
2020-03-04 22:57:33 +0600

    Initial Commit

---
+++ TextEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextEditor
+//
+//  Created by azamat on 3/4/20.
+//  Copyright © 2020 azamat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
